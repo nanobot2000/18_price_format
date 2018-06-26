d979dac14670e1439d277561732954ad3b2271a7
nanobot2000 <marginal3452@yandex.ru>
2018-06-26 23:47:47 +0300

    added parse_command_line_args and format_price functions

--- format_price.py
+++ format_price.py
@@ -1,5 +1,29 @@
+import argparse
+
+
 def format_price(price):
-    pass
+    try:
+        price = round(float(price), 2)
+        if float.is_integer(price):
+            price = int(price)
+    except (TypeError, ValueError):
+        return None
+    return '{:,}'.format(price).replace(',', ' ')
+
+
+def parse_command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p',
+        '--price',
+        type=int and float,
+        help='set price',
+    )
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == '__main__':
-    pass
+    args = parse_command_line_args()
+    output_price = format_price(args.price)
+    print(output_price)
